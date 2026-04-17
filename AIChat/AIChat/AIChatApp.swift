257e65cb20b95473cff435b95ff0be36ed111300
Juan Dorado <jfdoradotr@icloud.com>
2026-04-16 18:42:13 -0600

    Create the initial project

---
+++ AIChat/AIChat/AIChatApp.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
