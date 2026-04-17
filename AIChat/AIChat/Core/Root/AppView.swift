f036cd8d4133d89cb68a1b92a84aa128a2027235
Juan Dorado <jfdoradotr@icloud.com>
2026-04-16 22:43:34 -0600

    Move the state logic into an AppViewBuilder to change between views

--- AIChat/AIChat/Core/Root/AppView.swift
+++ AIChat/AIChat/Core/Root/AppView.swift
@@ -4,26 +4,44 @@
 
 import SwiftUI
 
-struct AppView: View {
-  @State private var showTabBar = false
+struct AppViewBuilder<TabBarView: View, OnboardingView: View>: View {
+  let showTabBar: Bool
+  @ViewBuilder let tabBarView: TabBarView
+  @ViewBuilder let onboardingView: OnboardingView
 
   var body: some View {
     ZStack {
       if showTabBar {
+        tabBarView
+          .transition(.move(edge: .trailing))
+      } else {
+        onboardingView
+          .transition(.move(edge: .leading))
+      }
+    }
+    .animation(.smooth, value: showTabBar)
+  }
+}
+
+struct AppView: View {
+  @State private var showTabBar = false
+
+  var body: some View {
+    AppViewBuilder(
+      showTabBar: showTabBar,
+      tabBarView: {
         ZStack {
           Color.red.ignoresSafeArea()
           Text("TabBar")
         }
-        .transition(.move(edge: .trailing))
-      } else {
+      },
+      onboardingView: {
         ZStack {
           Color.blue.ignoresSafeArea()
           Text("Onboarding")
         }
-        .transition(.move(edge: .leading))
       }
-    }
-    .animation(.smooth, value: showTabBar)
+    )
     .onTapGesture {
       showTabBar.toggle()
     }
