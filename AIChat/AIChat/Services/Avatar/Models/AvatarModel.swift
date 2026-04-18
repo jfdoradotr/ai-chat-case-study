f0ee2dcb072c100e444c9c0918a9fb881a36295c
Juan Dorado <jfdoradotr@icloud.com>
2026-04-18 13:28:28 -0600

    Add a description taking care of grammar

--- AIChat/AIChat/Services/Avatar/Models/AvatarModel.swift
+++ AIChat/AIChat/Services/Avatar/Models/AvatarModel.swift
@@ -13,6 +13,14 @@ struct AvatarModel {
   let authorId: String?
   let dateCreated: Date?
 
+  var description: String {
+    var components: [String] = []
+    if let character { components.append(character.phrase) }
+    if let action { components.append("that is \(action.rawValue)") }
+    if let location { components.append(location.phrase) }
+    return components.joined(separator: " ")
+  }
+
   init(
     avatarId: String,
     name: String? = nil,
@@ -33,20 +41,32 @@ struct AvatarModel {
 }
 
 extension AvatarModel {
-  enum Character {
+  enum Character: String {
     case man, woman, alien, dog, cat
+
+    var phrase: String {
+      let startsWithVowel = rawValue.lowercased().first.map { "aeiou".contains($0) } ?? false
+      return "\(startsWithVowel ? "An" : "A") \(rawValue)"
+    }
   }
 }
 
 extension AvatarModel {
-  enum Action {
+  enum Action: String {
     case smiling, sitting, eating, drinking, walking, shopping, studying, working, relaxing, fighting, crying
   }
 }
 
 extension AvatarModel {
-  enum Location {
+  enum Location: String {
     case park, mall, museum, city, dessert, forest, space
+
+    var phrase: String {
+      switch self {
+      case .space: return "in space"
+      default: return "in the \(rawValue)"
+      }
+    }
   }
 }
 
