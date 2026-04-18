325e3f86b6aa792fd36b0a73593a8269db47ad21
Juan Dorado <jfdoradotr@icloud.com>
2026-04-18 13:20:17 -0600

    Create initializer to avoid nil parameters

--- AIChat/AIChat/Services/Avatar/Models/AvatarModel.swift
+++ AIChat/AIChat/Services/Avatar/Models/AvatarModel.swift
@@ -6,12 +6,30 @@ import Foundation
 
 struct AvatarModel {
   let avatarId: String
-  let name: String
+  let name: String?
   let character: Character?
   let action: Action?
   let location: Location?
   let authorId: String?
   let dateCreated: Date?
+
+  init(
+    avatarId: String,
+    name: String? = nil,
+    character: Character? = nil,
+    action: Action? = nil,
+    location: Location? = nil,
+    authorId: String? = nil,
+    dateCreated: Date? = nil
+  ) {
+    self.avatarId = avatarId
+    self.name = name
+    self.character = character
+    self.action = action
+    self.location = location
+    self.authorId = authorId
+    self.dateCreated = dateCreated
+  }
 }
 
 extension AvatarModel {
