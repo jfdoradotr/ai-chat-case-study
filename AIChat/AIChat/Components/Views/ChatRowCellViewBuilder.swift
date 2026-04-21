b7dfa53d8ef0c9a8b869765c2cf8a7bea1c54d62
Juan Dorado <jfdoradotr@icloud.com>
2026-04-21 17:49:24 -0600

    Add more loading state

--- AIChat/AIChat/Components/Views/ChatRowCellViewBuilder.swift
+++ AIChat/AIChat/Components/Views/ChatRowCellViewBuilder.swift
@@ -7,24 +7,48 @@ import SwiftUI
 struct ChatRowCellViewBuilder: View {
   @State private var avatar: AvatarModel?
   @State private var lastChatMessage: ChatMessageModel?
+  @State private var didLoadAvatar = false
+  @State private var didLoadChatMessage = false
 
   var currentUserId: String? = ""
   var chat: ChatModel = .preview
   var getAvatar: () async -> AvatarModel?
   var getLastChatMessage: () async -> ChatMessageModel?
 
+  private var isLoading: Bool {
+    if didLoadAvatar && didLoadChatMessage {
+      return false
+    }
+    return true
+  }
+
+  private var subheadline: String? {
+    if isLoading {
+      return "xxxx xxxx xxxx xxxx"
+    }
+
+    if avatar == nil && lastChatMessage == nil {
+      return "Error loading data"
+    }
+
+    return  lastChatMessage?.content
+  }
+
   var body: some View {
     ChatRowCellView(
       imageURL: avatar?.imageURL,
-      headline: avatar?.name,
-      subheadline: lastChatMessage?.content,
-      hasNewChat: hasNewChat
+      headline: isLoading ? "xxxx xxxx" : avatar?.name,
+      subheadline: subheadline,
+      hasNewChat: isLoading ? false : hasNewChat
     )
+    .redacted(reason: isLoading ? .placeholder : [])
     .task {
       avatar = await getAvatar()
+      didLoadAvatar = true
     }
     .task {
       lastChatMessage = await getLastChatMessage()
+      didLoadChatMessage = true
     }
   }
 
@@ -35,9 +59,27 @@ struct ChatRowCellViewBuilder: View {
 }
 
 #Preview {
-  ChatRowCellViewBuilder(
-    chat: .preview,
-    getAvatar: { return .preview },
-    getLastChatMessage: { return .preview }
-  )
+  List {
+    ChatRowCellViewBuilder(
+      chat: .preview,
+      getAvatar: {
+        try? await Task.sleep(for: .seconds(5))
+        return .preview
+      },
+      getLastChatMessage: {
+        try? await Task.sleep(for: .seconds(5))
+        return .preview
+      }
+    )
+    ChatRowCellViewBuilder(
+      chat: .preview,
+      getAvatar: { return .preview },
+      getLastChatMessage: { return .preview }
+    )
+    ChatRowCellViewBuilder(
+      chat: .preview,
+      getAvatar: { return nil },
+      getLastChatMessage: { return nil }
+    )
+  }
 }
