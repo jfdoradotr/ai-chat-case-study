1bea199688fd302b306285640de4d73c6d2a6a1b
Juan Dorado <jfdoradotr@icloud.com>
2026-04-28 16:25:54 -0600

    create the bottom bar with the text and button

--- AIChat/AIChat/Core/Chat/ChatView.swift
+++ AIChat/AIChat/Core/Chat/ChatView.swift
@@ -8,29 +8,60 @@ struct ChatView: View {
   @State private var chatMesages: [ChatMessageModel] = .preview
   @State private var avatar: AvatarModel? = .preview
   @State private var currentUser: UserModel? = .preview
+  @State private var messageText: String = ""
 
   var body: some View {
     VStack(spacing: 0) {
-      ScrollView {
-        LazyVStack(spacing: 24) {
-          ForEach(chatMesages) { message in
-            let isCurrentUser = message.authorId == currentUser?.userId
-            ChatBubbleViewBuilder(
-              message: message,
-              isCurrentUser: isCurrentUser,
-              imageURL: avatar?.imageURL
-            )
-          }
-        }
-        .frame(maxWidth: .infinity)
-        .padding(.horizontal, 8)
-      }
+      scrollViewSection
 
-      Rectangle()
-        .frame(height: 50)
+      TextField("Say something...", text: $messageText)
+        .keyboardType(.alphabet)
+        .autocorrectionDisabled()
+        .padding(12)
+        .padding(.trailing, 40)
+        .overlay(
+          Button(action: onSendButtonTapped) {
+            Label("Send Message", systemImage: "arrow.up.circle.fill")
+              .labelStyle(.iconOnly)
+              .font(.largeTitle)
+          }.padding(.trailing, 4),
+          alignment: .trailing
+        )
+        .background(
+          ZStack {
+            RoundedRectangle(cornerRadius: 100)
+              .fill(Color(.systemBackground))
+            RoundedRectangle(cornerRadius: 100)
+              .stroke(Color.gray.opacity(0.03), lineWidth: 1)
+          }
+        )
+        .padding(.horizontal, 12)
+        .padding(.vertical, 6)
+        .background(Color(.secondarySystemBackground))
     }
     .navigationTitle(avatar?.name ?? "Chat")
     .toolbarTitleDisplayMode(.inline)
+  }
+
+  private var scrollViewSection: some View {
+    ScrollView {
+      LazyVStack(spacing: 24) {
+        ForEach(chatMesages) { message in
+          let isCurrentUser = message.authorId == currentUser?.userId
+          ChatBubbleViewBuilder(
+            message: message,
+            isCurrentUser: isCurrentUser,
+            imageURL: avatar?.imageURL
+          )
+        }
+      }
+      .frame(maxWidth: .infinity)
+      .padding(.horizontal, 8)
+    }
+  }
+
+  private func onSendButtonTapped() {
+
   }
 }
 
