46ec6fec6de98a0bd255a1edd098943795d1a08a
Juan Dorado <jfdoradotr@icloud.com>
2026-04-28 16:29:39 -0600

    Move textfield into its own section

--- AIChat/AIChat/Core/Chat/ChatView.swift
+++ AIChat/AIChat/Core/Chat/ChatView.swift
@@ -13,31 +13,7 @@ struct ChatView: View {
   var body: some View {
     VStack(spacing: 0) {
       scrollViewSection
-
-      TextField("Say something...", text: $messageText)
-        .keyboardType(.alphabet)
-        .autocorrectionDisabled()
-        .padding(12)
-        .padding(.trailing, 40)
-        .overlay(
-          Button(action: onSendButtonTapped) {
-            Label("Send Message", systemImage: "arrow.up.circle.fill")
-              .labelStyle(.iconOnly)
-              .font(.largeTitle)
-          }.padding(.trailing, 4),
-          alignment: .trailing
-        )
-        .background(
-          ZStack {
-            RoundedRectangle(cornerRadius: 100)
-              .fill(Color(.systemBackground))
-            RoundedRectangle(cornerRadius: 100)
-              .stroke(Color.gray.opacity(0.03), lineWidth: 1)
-          }
-        )
-        .padding(.horizontal, 12)
-        .padding(.vertical, 6)
-        .background(Color(.secondarySystemBackground))
+      textFieldSection
     }
     .navigationTitle(avatar?.name ?? "Chat")
     .toolbarTitleDisplayMode(.inline)
@@ -58,6 +34,33 @@ struct ChatView: View {
       .frame(maxWidth: .infinity)
       .padding(.horizontal, 8)
     }
+  }
+
+  private var textFieldSection: some View {
+    TextField("Say something...", text: $messageText)
+      .keyboardType(.alphabet)
+      .autocorrectionDisabled()
+      .padding(12)
+      .padding(.trailing, 40)
+      .overlay(
+        Button(action: onSendButtonTapped) {
+          Label("Send Message", systemImage: "arrow.up.circle.fill")
+            .labelStyle(.iconOnly)
+            .font(.largeTitle)
+        }.padding(.trailing, 4),
+        alignment: .trailing
+      )
+      .background(
+        ZStack {
+          RoundedRectangle(cornerRadius: 100)
+            .fill(Color(.systemBackground))
+          RoundedRectangle(cornerRadius: 100)
+            .stroke(Color.gray.opacity(0.03), lineWidth: 1)
+        }
+      )
+      .padding(.horizontal, 12)
+      .padding(.vertical, 6)
+      .background(Color(.secondarySystemBackground))
   }
 
   private func onSendButtonTapped() {
