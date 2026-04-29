212bc057b89e1ed3cb88358e8ecc0a4b745a9e56
Juan Dorado <jfdoradotr@icloud.com>
2026-04-29 00:20:38 -0600

    Create Profile Modal View

---
+++ AIChat/AIChat/Components/Modals/ProfileModalView.swift
@@ -0,0 +1,62 @@
+//
+//  Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileModalView: View {
+  var imageURL: URL? = Constants.randomImageURL
+  var title: String? = "Alpha"
+  var subtitle: String? = "Alien"
+  var headline: String? = "An alien in the park."
+  var onClosePressed: (() -> Void)
+
+  var body: some View {
+    VStack(spacing: 0) {
+      if let imageURL {
+        ImageLoaderView(url: imageURL)
+          .aspectRatio(1, contentMode: .fit)
+      }
+
+      VStack(alignment: .leading, spacing: 16) {
+        if let title {
+          Text(title)
+            .font(.title.weight(.semibold))
+        }
+
+        if let subtitle {
+          Text(subtitle)
+            .font(.title3)
+            .foregroundStyle(.secondary)
+        }
+
+        if let headline {
+          Text(headline)
+            .font(.subheadline)
+            .foregroundStyle(.secondary)
+        }
+      }
+      .padding(24)
+      .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    .background(.thinMaterial)
+    .clipShape(.rect(cornerRadius: 16))
+    .overlay(
+      Button(
+        "Close",
+        systemImage: "xmark.circle.fill",
+        action: onClosePressed
+      )
+      .labelStyle(.iconOnly)
+      .font(.title)
+      .foregroundStyle(.black)
+      .padding(4)
+      .padding(8),
+      alignment: .topTrailing
+    )
+  }
+}
+
+#Preview {
+  ProfileModalView {}
+}
