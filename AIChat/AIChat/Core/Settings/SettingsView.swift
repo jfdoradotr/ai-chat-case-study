79330702b57b77f61b3e3c79d73600ca5dd2efc9
Juan Dorado <jfdoradotr@icloud.com>
2026-04-29 22:21:40 -0600

    Extract in small computed views

--- AIChat/AIChat/Core/Settings/SettingsView.swift
+++ AIChat/AIChat/Core/Settings/SettingsView.swift
@@ -6,6 +6,7 @@ import SwiftUI
 
 struct SettingsView: View {
   @Environment(\.dismiss) private var dismiss
+  @Environment(\.authService) private var authService
   @Environment(AppState.self) private var appState
 
   @State private var isPremium = false
@@ -14,82 +15,98 @@ struct SettingsView: View {
 
   var body: some View {
     List {
-      Section {
-        if isAnonymousUser {
-          Button(
-            "Save & back-up account",
-            action: onCreateAccountPressed
-          )
-          .foregroundStyle(.primary)
-        } else {
-          Button(
-            "Sign out",
-            action: onSignOutPressed
-          )
-          .foregroundStyle(.primary)
-          Button(
-            "Delete account",
-            role: .destructive,
-            action: onDeleteAccountPressed
-          )
-        }
-      } header: {
-        Text("Account")
-      }
-      Section {
-        HStack {
-          Text("Account status: \(isPremium ? "PREMIUM" : "FREE")")
-          Spacer()
-          if isPremium {
-            Button("MANAGE", action: onManagePressed)
-              .font(.caption)
-              .bold()
-              .foregroundStyle(.white)
-              .padding(.horizontal, 8)
-              .padding(.vertical, 6)
-              .background(Color.blue)
-              .clipShape(.capsule)
-          }
-        }
-      } header: {
-        Text("Purchases")
-      }
-      Section {
-        HStack {
-          Text("Version")
-          Spacer()
-          Text(Bundle.main.appVersion)
-        }
-        HStack {
-          Text("Build Number")
-          Spacer()
-          Text(Bundle.main.buildNumber)
-        }
-        Button(
-          "Contact us",
-          action: onContactUsPressed
-        )
-        .foregroundStyle(.blue)
-      } header: {
-        Text("Application")
-      } footer: {
-        VStack(alignment: .leading) {
-          Text("Created by Swifty Journey.")
-          if let url = URL(string: "https://swiftyjourney.com") {
-            HStack(spacing: 0) {
-              Text("Learn more at ")
-              Link("www.swiftyjourney.com", destination: url)
-            }
-          }
-        }
-        .font(.footnote)
-      }
+      accountSection
+      purchaseSection
+      applicationSection
     }
     .navigationTitle("Settings")
     .sheet(isPresented: $showCreateAccount) {
       CreateAccountView(presentationState: .createAccount)
         .presentationDetents([.medium])
     }
+  }
+
+  private var accountSection: some View {
+    Section {
+      if isAnonymousUser {
+        Button(
+          "Save & back-up account",
+          action: onCreateAccountPressed
+        )
+        .foregroundStyle(.primary)
+      } else {
+        Button(
+          "Sign out",
+          action: onSignOutPressed
+        )
+        .foregroundStyle(.primary)
+        Button(
+          "Delete account",
+          role: .destructive,
+          action: onDeleteAccountPressed
+        )
+      }
+    } header: {
+      Text("Account")
+    }
+  }
+
+  private var purchaseSection: some View {
+    Section {
+      HStack {
+        Text("Account status: \(isPremium ? "PREMIUM" : "FREE")")
+        Spacer()
+        if isPremium {
+          Button("MANAGE", action: onManagePressed)
+            .font(.caption)
+            .bold()
+            .foregroundStyle(.white)
+            .padding(.horizontal, 8)
+            .padding(.vertical, 6)
+            .background(Color.blue)
+            .clipShape(.capsule)
+        }
+      }
+    } header: {
+      Text("Purchases")
+    }
+  }
+
+  private var applicationSection: some View {
+    Section {
+      HStack {
+        Text("Version")
+        Spacer()
+        Text(Bundle.main.appVersion)
+      }
+      HStack {
+        Text("Build Number")
+        Spacer()
+        Text(Bundle.main.buildNumber)
+      }
+      Button(
+        "Contact us",
+        action: onContactUsPressed
+      )
+      .foregroundStyle(.blue)
+    } header: {
+      Text("Application")
+    } footer: {
+      VStack(alignment: .leading) {
+        Text("Created by Swifty Journey.")
+        if let url = URL(string: "https://swiftyjourney.com") {
+          HStack(spacing: 0) {
+            Text("Learn more at ")
+            Link("www.swiftyjourney.com", destination: url)
+          }
+        }
+      }
+      .font(.footnote)
+    }
+  }
+
+  func setAnonymousAccountStatus() {
+    isAnonymousUser = authService.getAuthenticatedUser()?.isAnonymous == true
   }
 
   private func onSignOutPressed() {
