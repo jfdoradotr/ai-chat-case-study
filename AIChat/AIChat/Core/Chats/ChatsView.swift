9acec77e5ae6c2f966a080ce79c7de7c40c933d7
Juan Dorado <jfdoradotr@icloud.com>
2026-04-29 21:15:41 -0600

    Create the recent avatars section

--- AIChat/AIChat/Core/Chats/ChatsView.swift
+++ AIChat/AIChat/Core/Chats/ChatsView.swift
@@ -5,32 +5,79 @@
 import SwiftUI
 
 struct ChatsView: View {
-  let chats: [ChatModel] = .preview
+  @State private var chats: [ChatModel] = .preview
+  @State private var recentAvatars: [AvatarModel] = .preview
 
   var body: some View {
     List {
-      ForEach(chats) { chat in
-        NavigationLink(value: chat.avatarId) {
-          ChatRowCellViewBuilder(
-            currentUserId: nil, // FIXME: Add current user id
-            chat: chat,
-            getAvatar: {
-              try? await Task.sleep(for: .seconds(1))
-              return [AvatarModel].preview.randomElement()
-            },
-            getLastChatMessage: {
-              try? await Task.sleep(for: .seconds(1))
-              return [ChatMessageModel].preview.randomElement()
+      if !recentAvatars.isEmpty {
+        Section {
+          ScrollView(.horizontal) {
+            LazyHStack(spacing: 8) {
+              ForEach(recentAvatars) { avatar in
+                NavigationLink(value: avatar.avatarId) {
+                  if let imageURL = avatar.imageURL {
+                    VStack(spacing: 8) {
+                      ImageLoaderView(url: imageURL)
+                        .aspectRatio(1, contentMode: .fit)
+                        .clipShape(.circle)
+
+                      Text(avatar.name ?? "")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                    }
+                  }
+                }
+              }
             }
-          )
-          .listRowSeparator(.hidden)
+          }
+          .frame(height: 120)
+          .scrollIndicators(.hidden)
+        } header: {
+          Text("Recents")
         }
+        .listRowSeparator(.hidden)
       }
+      chatsSection
     }
     .listStyle(.plain)
     .navigationTitle("Chats")
     .navigationDestination(for: String.self) { avatarId in
       ChatView(avatarId: avatarId)
+    }
+  }
+
+  private var chatsSection: some View {
+    Section {
+      if chats.isEmpty {
+        Text("Your chats will appear here!")
+          .foregroundStyle(.secondary)
+          .font(.title3)
+          .frame(maxWidth: .infinity)
+          .multilineTextAlignment(.center)
+          .padding(40)
+          .listRowSeparator(.hidden)
+      } else {
+        ForEach(chats) { chat in
+          NavigationLink(value: chat.avatarId) {
+            ChatRowCellViewBuilder(
+              currentUserId: nil, // FIXME: Add current user id
+              chat: chat,
+              getAvatar: {
+                try? await Task.sleep(for: .seconds(1))
+                return [AvatarModel].preview.randomElement()
+              },
+              getLastChatMessage: {
+                try? await Task.sleep(for: .seconds(1))
+                return [ChatMessageModel].preview.randomElement()
+              }
+            )
+            .listRowSeparator(.hidden)
+          }
+        }
+      }
+    } header: {
+      Text("Chats")
     }
   }
 }
